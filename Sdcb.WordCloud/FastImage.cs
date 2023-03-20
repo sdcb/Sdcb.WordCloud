abc4f63061270d41c59f7320d806f6b8a3e3e185
sdcb <sdflysha@qq.com>
2023-03-20 14:37:54 +0800

    format

--- Sdcb.WordCloud/FastImage.cs
+++ Sdcb.WordCloud/FastImage.cs
@@ -5,39 +5,40 @@ using System.Runtime.InteropServices;
 
 namespace Sdcb.WordClouds
 {
-	/// <summary>
-	/// Image class for fast manipulation of bitmap data.
-	/// </summary>
-	internal class FastImage : IDisposable
-	{
-		public FastImage(int width, int height, PixelFormat format)
-		{
-			PixelFormatSize = Image.GetPixelFormatSize(format) / 8;
-			Stride = width*PixelFormatSize;
+    /// <summary>
+    /// Image class for fast manipulation of bitmap data.
+    /// </summary>
+    internal class FastImage : IDisposable
+    {
+        public FastImage(int width, int height, PixelFormat format)
+        {
+            PixelFormatSize = Image.GetPixelFormatSize(format) / 8;
+            Stride = width * PixelFormatSize;
 
-			Data = new byte[Stride * height];
-			Handle = GCHandle.Alloc(Data, GCHandleType.Pinned);
-			IntPtr pData = Marshal.UnsafeAddrOfPinnedArrayElement(Data, 0);
-			Bitmap = new Bitmap(width, height, Stride, format, pData);
-		}
+            Data = new byte[Stride * height];
+            Handle = GCHandle.Alloc(Data, GCHandleType.Pinned);
+            IntPtr pData = Marshal.UnsafeAddrOfPinnedArrayElement(Data, 0);
+            Bitmap = new Bitmap(width, height, Stride, format, pData);
+        }
 
-		public int Width { get { return Bitmap.Width; } }
+        public int Width { get { return Bitmap.Width; } }
 
-		public int Height { get { return Bitmap.Height; } }
+        public int Height { get { return Bitmap.Height; } }
 
-		public int PixelFormatSize { get; set; }
+        public int PixelFormatSize { get; set; }
 
-		public GCHandle Handle { get; set; }
+        public GCHandle Handle { get; set; }
 
-		public int Stride { get; set; }
+        public int Stride { get; set; }
 
-		public byte[] Data { get; set; }
+        public byte[] Data { get; set; }
 
-		public Bitmap Bitmap { get; set; }
-		public void Dispose()
-		{
-			Handle.Free();
-			Bitmap.Dispose();
-		}
-	}
+        public Bitmap Bitmap { get; set; }
+
+        public void Dispose()
+        {
+            Handle.Free();
+            Bitmap.Dispose();
+        }
+    }
 }
