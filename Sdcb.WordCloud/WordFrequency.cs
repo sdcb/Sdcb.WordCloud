1986f295027050897a6f2a8d27e26829cf022070
sdcb <sdflysha@qq.com>
2024-01-09 20:15:54 +0800

    Added initial project.

---
+++ Sdcb.WordCloud/WordFrequency.cs
@@ -0,0 +1,8 @@
+﻿namespace Sdcb.WordCloud;
+
+/// <summary>
+/// Word and frequency pair
+/// </summary>
+/// <param name="Word">word ordered by occurance</param>
+/// <param name="Frequency">frequecy</param>
+public record WordFrequency(string Word, int Frequency);
---
+++ Sdcb.WordCloud2.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Sdcb.WordCloud2.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
