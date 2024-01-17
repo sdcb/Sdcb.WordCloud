136dd05d0aaf07bc12ddff56fc2d1ee03a35d4ce
sdcb <sdflysha@qq.com>
2024-01-17 13:07:30 +0800

    complete MaskOptions

--- Sdcb.WordCloud/MaskOptions.cs
+++ Sdcb.WordCloud/MaskOptions.cs
@@ -3,23 +3,62 @@ using System;
 
 namespace Sdcb.WordClouds;
 
-public record MaskOptions(SKBitmap Mask, SKColor? AllowedFillColor, SKColor? BlockedFillColor)
+/// <summary>
+/// Represents the mask options for creating a word cloud, specifying which areas of the mask are fillable.
+/// </summary>
+/// <remarks>
+/// <para>The Mask property supports these pixel formats:</para>
+/// <list type="bullet">
+/// <item><see cref="SKColorType.Gray8"/></item>
+/// <item><see cref="SKColorType.Alpha8"/></item>
+/// <item><see cref="SKColorType.Bgra8888"/></item>
+/// <item><see cref="SKColorType.Rgba8888"/></item>
+/// <item><see cref="SKColorType.Rgb888x"/></item>
+/// </list>
+/// <para>Note that other pixel formats are not supported.</para>
+/// <para>Either BackgroundColor or ForegroundColor can be set, but not both; if both are provided, only ForegroundColor will be used.</para>
+/// <para>If BackgroundColor is not null, areas of the mask image that match the BackgroundColor are identified as fillable areas,
+/// and any other color is considered non-fillable. This is suitable for scenarios where the background has been pre-marked.</para>
+/// <para>If ForegroundColor is not null, then areas of the mask image that match the ForegroundColor are identified as 
+/// non-fillable areas, and any other color is regarded as fillable. This is suitable for scenarios where the foreground has been pre-marked.</para>
+/// </remarks>
+/// <param name="Mask">The mask bitmap image to define fillable areas for word cloud generation.</param>
+/// <param name="BackgroundColor">The color in the mask image representing the fillable areas; null to ignore this option.</param>
+/// <param name="ForegroundColor">The color in the mask image representing non-fillable areas; null to ignore this option.</param>
+
+public record MaskOptions(SKBitmap Mask, SKColor? BackgroundColor, SKColor? ForegroundColor)
 {
-    public MaskOptions(SKBitmap Mask) : this(Mask, AllowedFillColor: default(SKColor), BlockedFillColor: null)
+    /// <summary>
+    /// Initializes a new instance of the MaskOptions record with transparent background color.
+    /// </summary>
+    /// <param name="Mask">The mask bitmap image to define fillable areas for word cloud generation.</param>
+    public MaskOptions(SKBitmap Mask) : this(Mask, BackgroundColor: SKColors.Transparent, ForegroundColor: null)
     {
     }
 
-    public static MaskOptions CreateWithAllowedFillColor(SKBitmap mask, SKColor allowedFillColor)
+    /// <summary>
+    /// Creates a new MaskOptions instance specifying a background color as the allowed fillable color.
+    /// </summary>
+    /// <param name="mask">The mask bitmap image to use for generating the word cloud.</param>
+    /// <param name="backgroundColor">The color representing the fillable areas within the mask image.</param>
+    /// <returns>A new MaskOptions record configured with the allowed background fill color.</returns>
+    public static MaskOptions CreateWithBackgroundColor(SKBitmap mask, SKColor backgroundColor)
     {
-        return new(mask, allowedFillColor, null);
+        return new(mask, backgroundColor, null);
     }
 
-    public static MaskOptions CreateWithBlockedFillColor(SKBitmap mask, SKColor blockedFillColor)
+    /// <summary>
+    /// Creates a new MaskOptions instance specifying a foreground color as the blocked non-fillable color.
+    /// </summary>
+    /// <param name="mask">The mask bitmap image to use for generating the word cloud.</param>
+    /// <param name="foregroundColor">The color representing the non-fillable areas within the mask image.</param>
+    /// <returns>A new MaskOptions record configured with the specified foreground color to block filling.</returns>
+    public static MaskOptions CreateWithForegroundColor(SKBitmap mask, SKColor foregroundColor)
     {
-        return new(mask, null, blockedFillColor);
+        return new(mask, null, foregroundColor);
     }
 
-    internal unsafe void FillMaskCache(int width, int height, bool[] cache)
+    public void FillMaskCache(int width, int height, bool[] cache)
     {
         if (Mask.Width < width || Mask.Height < height)
         {
@@ -33,86 +72,35 @@ public record MaskOptions(SKBitmap Mask, SKColor? AllowedFillColor, SKColor? Blo
 
         if (Mask.ColorType == SKColorType.Alpha8 || Mask.ColorType == SKColorType.Gray8)
         {
-            U8MaskToCache(cache, width, height, this);
+            FillMaskCacheU8(cache, width, height);
         }
-        else if (Mask.ColorType == SKColorType.Bgra8888)
+        else if (Mask.ColorType == SKColorType.Bgra8888 || Mask.ColorType == SKColorType.Rgba8888 || Mask.ColorType == SKColorType.Rgb888x)
         {
-            Bgra8888MaskToCache(cache, width, height, this);
+            FillMaskCacheBgra8888(cache, width, height);
         }
         else
         {
-            using SKBitmap tempMask = ConvertColor(Mask, SKColorType.Gray8, SKAlphaType.Unknown);
-            U8MaskToCache(cache, width, height, this with { Mask = tempMask });
+            throw new ArgumentException("Mask color type must be Alpha8, Gray8, Bgra8888, Rgba8888, or Rgb888x.");
         }
     }
 
-    private static int ConvertSKColorToInt32(SKColor color, SKColorType maskColorType)
+    internal unsafe void FillMaskCacheBgra8888(bool[] cache, int width, int height)
     {
-        // 转换颜色格式，将 SKColor 拆分为对应的通道
-        byte red = color.Red;
-        byte green = color.Green;
-        byte blue = color.Blue;
-        byte alpha = color.Alpha;
-
-        switch (maskColorType)
-        {
-            case SKColorType.Rgba8888:
-                // RGBA8888 格式的32位整数
-                return (alpha << 24) | (blue << 16) | (green << 8) | red;
-
-            case SKColorType.Bgra8888:
-                // BGRA8888 格式的32位整数
-                return (alpha << 24) | (red << 16) | (green << 8) | blue;
-
-            case SKColorType.Rgb888x:
-                // RGB888x 格式是没有Alpha通道的RGB颜色，x代表无用通道
-                return (0xFF << 24) | (blue << 16) | (green << 8) | red;
-
-            default:
-                // 如果不是上述指定的格式，则抛出异常
-                throw new ArgumentOutOfRangeException(nameof(maskColorType),
-                    "Mask color type must be Rgba8888, Bgra8888, or Rgb888x.");
-        }
-    }
-
-    private static byte ConvertSKColorToGray8(SKColor color, SKColorType maskColorType)
-    {
-        if (maskColorType == SKColorType.Gray8)
-        {
-            // 根据亮度计算公式计算灰度值
-            // Y' = 0.299 R + 0.587 G + 0.114 B
-            float gray = 0.299f * color.Red + 0.587f * color.Green + 0.114f * color.Blue;
-
-            // 将计算结果转换为byte
-            return (byte)gray;
-        }
-        else if (maskColorType == SKColorType.Alpha8)
-        {
-            return color.Alpha;
-        }
-        else
-        {
-            throw new ArgumentOutOfRangeException(nameof(maskColorType), "Mask color type must be Gray8 or Alpha8.");
-        }
-    }
-
-    private unsafe void Bgra8888MaskToCache(bool[] cache, int width, int height, MaskOptions maskOptions)
-    {
-        int* ptr = (int*)maskOptions.Mask.GetPixels();
+        int* ptr = (int*)Mask.GetPixels();
         fixed (bool* dest = cache)
         {
             int size = width * height;
-            if (maskOptions.AllowedFillColor.HasValue)
+            if (BackgroundColor.HasValue)
             {
-                int i32 = ConvertSKColorToInt32(maskOptions.AllowedFillColor.Value, maskOptions.Mask.ColorType);
+                int i32 = ConvertSKColorToInt32(BackgroundColor.Value, Mask.ColorType);
                 for (int i = 0; i < size; i++)
                 {
                     dest[i] = ptr[i] != i32;
                 }
             }
-            else if (maskOptions.BlockedFillColor != null)
+            else if (ForegroundColor != null)
             {
-                int i32 = ConvertSKColorToInt32(maskOptions.BlockedFillColor.Value, maskOptions.Mask.ColorType);
+                int i32 = ConvertSKColorToInt32(ForegroundColor.Value, Mask.ColorType);
                 for (int i = 0; i < size; i++)
                 {
                     dest[i] = ptr[i] == i32;
@@ -121,23 +109,23 @@ public record MaskOptions(SKBitmap Mask, SKColor? AllowedFillColor, SKColor? Blo
         }
     }
 
-    private unsafe static bool[] U8MaskToCache(bool[] cache, int width, int height, MaskOptions mask)
+    internal unsafe bool[] FillMaskCacheU8(bool[] cache, int width, int height)
     {
-        byte* ptr = (byte*)mask.Mask.GetPixels();
+        byte* ptr = (byte*)Mask.GetPixels();
         fixed (bool* dest = cache)
         {
             int size = width * height;
-            if (mask.AllowedFillColor.HasValue)
+            if (BackgroundColor.HasValue)
             {
-                byte gray8 = ConvertSKColorToGray8(mask.AllowedFillColor.Value, mask.Mask.ColorType);
+                byte gray8 = ConvertSKColorToU8(BackgroundColor.Value, Mask.ColorType);
                 for (int i = 0; i < size; i++)
                 {
                     dest[i] = ptr[i] != gray8;
                 }
             }
-            else if (mask.BlockedFillColor != null)
+            else if (ForegroundColor != null)
             {
-                byte gray8 = ConvertSKColorToGray8(mask.BlockedFillColor.Value, mask.Mask.ColorType);
+                byte gray8 = ConvertSKColorToU8(ForegroundColor.Value, Mask.ColorType);
                 for (int i = 0; i < size; i++)
                 {
                     dest[i] = ptr[i] == gray8;
@@ -155,5 +143,48 @@ public record MaskOptions(SKBitmap Mask, SKColor? AllowedFillColor, SKColor? Blo
         canvas.DrawBitmap(bmp, 0, 0);
 
         return result;
+    }
+
+    internal static int ConvertSKColorToInt32(SKColor color, SKColorType maskColorType)
+    {
+        if (color.Alpha == 0) return 0;
+
+        // 转换颜色格式，将 SKColor 拆分为对应的通道
+        byte red = color.Red;
+        byte green = color.Green;
+        byte blue = color.Blue;
+        byte alpha = color.Alpha;
+
+        return maskColorType switch
+        {
+            SKColorType.Rgba8888 => (alpha << 24) | (blue << 16) | (green << 8) | red,// RGBA8888 格式的32位整数
+            SKColorType.Bgra8888 => (alpha << 24) | (red << 16) | (green << 8) | blue,// BGRA8888 格式的32位整数
+            SKColorType.Rgb888x => (0xFF << 24) | (blue << 16) | (green << 8) | red,// RGB888x 格式是没有Alpha通道的RGB颜色，x代表无用通道
+            _ => throw new ArgumentOutOfRangeException(nameof(maskColorType),
+                "Mask color type must be Rgba8888, Bgra8888, or Rgb888x."),// 如果不是上述指定的格式，则抛出异常
+        };
+    }
+
+    private static byte ConvertSKColorToU8(SKColor color, SKColorType maskColorType)
+    {
+        if (color.Alpha == 0) return 0;
+
+        if (maskColorType == SKColorType.Gray8)
+        {
+            // 根据亮度计算公式计算灰度值
+            // Y' = 0.299 R + 0.587 G + 0.114 B
+            float gray = 0.299f * color.Red + 0.587f * color.Green + 0.114f * color.Blue;
+
+            // 将计算结果转换为byte
+            return (byte)gray;
+        }
+        else if (maskColorType == SKColorType.Alpha8)
+        {
+            return color.Alpha;
+        }
+        else
+        {
+            throw new ArgumentOutOfRangeException(nameof(maskColorType), "Mask color type must be Gray8 or Alpha8.");
+        }
     }
 }
---
+++ Sdcb.WordCloud2.Tests/MaskToCacheTest.cs
@@ -0,0 +1,259 @@
+using Sdcb.WordClouds;
+using SkiaSharp;
+using System.Buffers.Binary;
+
+namespace Sdcb.WordCloud2.Tests;
+
+public class MaskToCacheTest
+{
+    [Fact]
+    public unsafe void Gray8Default()
+    {
+        int width = 8, height = 6;
+        using SKBitmap bmp = new(width, height, SKColorType.Gray8, SKAlphaType.Unknown);
+        byte* pbmp = (byte*)bmp.GetPixels();
+        for (int i = 0; i < 5; ++i)
+        {
+            pbmp[Random.Shared.Next(width * height)] = (byte)Random.Shared.Next(1, 256);
+        }
+
+        byte[] bmpData = bmp.GetPixelSpan().ToArray();
+        MaskOptions mask = new(bmp);
+
+        bool[] data = new bool[width * height];
+        mask.FillMaskCache(width, height, data);
+        Assert.Equal(width * height, data.Length);
+        Assert.Equal(bmpData.Select(x => x > 0), data);
+    }
+
+    [Fact]
+    public unsafe void Gray8_Black()
+    {
+        int width = 8, height = 6;
+        using SKBitmap bmp = new(width, height, SKColorType.Gray8, SKAlphaType.Unknown);
+        byte* pbmp = (byte*)bmp.GetPixels();
+        for (int i = 0; i < 5; ++i)
+        {
+            pbmp[Random.Shared.Next(width * height)] = (byte)Random.Shared.Next(1, 256);
+        }
+
+        byte[] bmpData = bmp.GetPixelSpan().ToArray();
+        MaskOptions mask = MaskOptions.CreateWithBackgroundColor(bmp, SKColors.Black);
+
+        bool[] data = new bool[width * height];
+        mask.FillMaskCache(width, height, data);
+        Assert.Equal(width * height, data.Length);
+        Assert.Equal(bmpData.Select(x => x > 0), data);
+    }
+
+    [Fact]
+    public unsafe void Alpha8()
+    {
+        int width = 8, height = 6;
+        using SKBitmap bmp = new(width, height, SKColorType.Alpha8, SKAlphaType.Premul);
+        byte* pbmp = (byte*)bmp.GetPixels();
+        for (int i = 0; i < 5; ++i)
+        {
+            pbmp[Random.Shared.Next(width * height)] = (byte)Random.Shared.Next(1, 256);
+        }
+        MaskOptions mask = MaskOptions.CreateWithBackgroundColor(bmp, SKColors.Transparent);
+        bool[] cache = new bool[width * height];
+        mask.FillMaskCache(width, height, cache);
+
+        byte[] bmpData = bmp.GetPixelSpan().ToArray();
+        Assert.Equal(width * height, cache.Length);
+        Assert.Equal(bmpData.Select(x => x > 0), cache);
+    }
+
+    [Fact]
+    public unsafe void Transparent()
+    {
+        // arrange
+        int width = 8, height = 6;
+        using SKBitmap bmp = new(width, height, SKColorType.Bgra8888, SKAlphaType.Premul);
+
+        byte* pbmp = (byte*)bmp.GetPixels();
+        for (int i = 0; i < 5; ++i)
+        {
+            int pixelId = Random.Shared.Next(width * height);
+            pbmp[pixelId * 4 + Random.Shared.Next(0, 3)] = (byte)Random.Shared.Next(1, 256);
+        }
+        MaskOptions mask = MaskOptions.CreateWithBackgroundColor(bmp, SKColors.Transparent);
+
+        bool[] cache = new bool[width * height];
+
+        // act
+        mask.FillMaskCache(width, height, cache);
+
+        // assert
+        int[] bmpData = bmp.GetPixelSpan().ToArray().Chunk(4).Select(x => BitConverter.ToInt32(x)).ToArray();
+        Assert.Equal(width * height, cache.Length);
+        Assert.Equal(bmpData.Select(x => x > 0), cache);
+    }
+
+    [Fact]
+    public unsafe void BackgroundBlack()
+    {
+        // arrange
+        int width = 8, height = 6;
+        using SKBitmap bmp = new(width, height, SKColorType.Bgra8888, SKAlphaType.Premul);
+        using (SKCanvas canvas = new(bmp))
+        {
+            canvas.Clear(SKColors.Black);
+        }
+
+        byte* pbmp = (byte*)bmp.GetPixels();
+        for (int i = 0; i < 5; ++i)
+        {
+            int pixelId = Random.Shared.Next(width * height);
+            pbmp[pixelId * 4 + Random.Shared.Next(0, 3)] = (byte)Random.Shared.Next(1, 256);
+        }
+        MaskOptions mask = MaskOptions.CreateWithBackgroundColor(bmp, SKColors.Black);
+
+        bool[] cache = new bool[width * height];
+
+        // act
+        mask.FillMaskCache(width, height, cache);
+
+        // assert
+        int[] bmpData = bmp.GetPixelSpan().ToArray().Chunk(4)
+            .Select(x => MaskOptions.ConvertSKColorToInt32(BinaryPrimitives.ReadUInt32LittleEndian(x), SKColorType.Bgra8888))
+            .ToArray();
+        int black = MaskOptions.ConvertSKColorToInt32(SKColors.Black, SKColorType.Bgra8888);
+        Assert.Equal(width * height, cache.Length);
+        Assert.Equal(bmpData.Select(x => x != black), cache);
+    }
+
+    [Fact]
+    public unsafe void Bgra8888_Red()
+    {
+        // arrange
+        int width = 8, height = 6;
+        using SKBitmap bmp = new(width, height, SKColorType.Bgra8888, SKAlphaType.Premul);
+        using (SKCanvas canvas = new(bmp))
+        {
+            canvas.Clear(SKColors.Red);
+        }
+
+        byte* pbmp = (byte*)bmp.GetPixels();
+        for (int i = 0; i < 5; ++i)
+        {
+            int pixelId = Random.Shared.Next(width * height);
+            pbmp[pixelId * 4 + Random.Shared.Next(0, 3)] = (byte)Random.Shared.Next(1, 254);
+        }
+        MaskOptions mask = MaskOptions.CreateWithBackgroundColor(bmp, SKColors.Red);
+
+        bool[] cache = new bool[width * height];
+
+        // act
+        mask.FillMaskCache(width, height, cache);
+
+        // assert
+        int[] bgraData = bmp.GetPixelSpan().ToArray().Chunk(4)
+            .Select(x => MaskOptions.ConvertSKColorToInt32(BinaryPrimitives.ReadUInt32LittleEndian(x), SKColorType.Bgra8888))
+            .ToArray();
+        int backgroundBgra = MaskOptions.ConvertSKColorToInt32(SKColors.Red, SKColorType.Bgra8888);
+        Assert.Equal(width * height, cache.Length);
+        Assert.Equal(bgraData.Select(x => x != backgroundBgra), cache);
+    }
+
+    [Fact]
+    public unsafe void Rgba8888_Red()
+    {
+        // arrange
+        int width = 8, height = 6;
+        using SKBitmap bmp = new(width, height, SKColorType.Rgba8888, SKAlphaType.Premul);
+        using (SKCanvas canvas = new(bmp))
+        {
+            canvas.Clear(SKColors.Red);
+        }
+
+        byte* pbmp = (byte*)bmp.GetPixels();
+        for (int i = 0; i < 5; ++i)
+        {
+            int pixelId = Random.Shared.Next(width * height);
+            pbmp[pixelId * 4 + Random.Shared.Next(0, 3)] = (byte)Random.Shared.Next(1, 254);
+        }
+        MaskOptions mask = MaskOptions.CreateWithBackgroundColor(bmp, SKColors.Red);
+
+        bool[] cache = new bool[width * height];
+
+        // act
+        mask.FillMaskCache(width, height, cache);
+
+        // assert
+        int[] bmpData = bmp.GetPixelSpan().ToArray().Chunk(4)
+            .Select(x => MaskOptions.ConvertSKColorToInt32(BinaryPrimitives.ReadUInt32LittleEndian(x), SKColorType.Rgba8888))
+            .ToArray();
+        int backgroundBgra = MaskOptions.ConvertSKColorToInt32(SKColors.Red, SKColorType.Bgra8888);
+        Assert.Equal(width * height, cache.Length);
+        Assert.Equal(bmpData.Select(x => x != backgroundBgra), cache);
+    }
+
+    [Fact]
+    public unsafe void Rgba8888_Red_Foreground()
+    {
+        // arrange
+        int width = 5, height = 3;
+        using SKBitmap bmp = new(width, height, SKColorType.Rgba8888, SKAlphaType.Premul);
+        int foregroundColor = MaskOptions.ConvertSKColorToInt32(SKColors.Red, SKColorType.Rgba8888);
+        int* pbmp = (int*)bmp.GetPixels();
+        for (int i = 0; i < 5; ++i)
+        {
+            int pixelId = Random.Shared.Next(width * height);
+            pbmp[pixelId] = foregroundColor;
+        }
+        MaskOptions mask = MaskOptions.CreateWithForegroundColor(bmp, SKColors.Red);
+        bool[] cache = new bool[width * height];
+
+        // act
+        mask.FillMaskCache(width, height, cache);
+
+        // assert
+        int[] bmpData = bmp.GetPixelSpan().ToArray().Chunk(4)
+            .Select(x => (int)BinaryPrimitives.ReadUInt32LittleEndian(x))
+            .ToArray();
+        
+        Assert.Equal(width * height, cache.Length);
+        Assert.Equal(bmpData.Select(x => x == foregroundColor), cache);
+    }
+
+    [Fact]
+    public unsafe void Rgba888x_Blue_Foreground()
+    {
+        // arrange
+        int width = 5, height = 3;
+        using SKBitmap bmp = new(width, height, SKColorType.Rgb888x, SKAlphaType.Premul);
+        SKColor foregroundColor = SKColors.Blue;
+        int foregroundColorI32 = MaskOptions.ConvertSKColorToInt32(foregroundColor, SKColorType.Rgb888x);
+        int* pbmp = (int*)bmp.GetPixels();
+        for (int i = 0; i < 5; ++i)
+        {
+            int pixelId = Random.Shared.Next(width * height);
+            pbmp[pixelId] = foregroundColorI32;
+        }
+        MaskOptions mask = MaskOptions.CreateWithForegroundColor(bmp, foregroundColor);
+        bool[] cache = new bool[width * height];
+
+        // act
+        mask.FillMaskCache(width, height, cache);
+
+        // assert
+        int[] bmpData = bmp.GetPixelSpan().ToArray().Chunk(4)
+            .Select(x => (int)BinaryPrimitives.ReadUInt32LittleEndian(x))
+            .ToArray();
+
+        Assert.Equal(width * height, cache.Length);
+        Assert.Equal(bmpData.Select(x => x == foregroundColorI32), cache);
+    }
+
+    [Fact]
+    public unsafe void Other_NotSupported()
+    {
+        int width = 5, height = 3;
+        using SKBitmap bmp = new(width, height, SKColorType.Bgr101010x, SKAlphaType.Premul);
+        MaskOptions mask = MaskOptions.CreateWithBackgroundColor(bmp, SKColors.Transparent);
+        bool[] cache = new bool[width * height];
+        Assert.Throws<ArgumentException>(() => mask.FillMaskCache(width, height, cache));
+    }
+}
